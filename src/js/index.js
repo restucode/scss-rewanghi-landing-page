ad1cd2f8f4cc4b96270cfb38c00d8c0f59020203
Restu <restu.kersana1@gmail.com>
2021-01-23 14:57:05 +0700

    fix hero and add support

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,26 @@
 import '../scss/main.scss'
 
 
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('.nav')
+const burger = document.querySelector('.nav-burger')
+const menu = document.querySelector('.nav-menu')
 
 window.addEventListener('scroll', function() {
  nav.classList.toggle('active', window.scrollY > 0)
+})
+
+let prevScrollpos = window.pageYOffset;
+window.addEventListener('scroll', function() {
+ let currentScrollPos = window.pageYOffset;
+ if (prevScrollpos > currentScrollPos) {
+  nav.classList.remove('hide') 
+ } else {
+  nav.classList.add('hide')
+ }
+ prevScrollpos = currentScrollPos;
+})
+
+burger.addEventListener('click', function() {
+ menu.classList.toggle('active')
+ document.body.classList.toggle('active')
 })
