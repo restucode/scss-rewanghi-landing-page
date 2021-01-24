970ddaedba9c24330ed5b558a63fe71f70c7599d
Restu <restu.kersana1@gmail.com>
2021-01-24 15:49:01 +0700

    replace

--- src/js/index.js
+++ src/js/index.js
@@ -55,30 +55,3 @@ clickMenuScroll.forEach(link => {
  })
 })
 
-
-// Observer API
-
-const sliders = document.querySelectorAll(".slide-in");
-
-// const appearOptions = {
-//   threshold: 0,
-//   rootMargin: "0px 0px -250px 0px"
-// };
-
-const appearOnScroll = new IntersectionObserver(function(
-  entries,
-  appearOnScroll
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-});
-
-sliders.forEach(slider => {
-  appearOnScroll.observe(slider);
-});
